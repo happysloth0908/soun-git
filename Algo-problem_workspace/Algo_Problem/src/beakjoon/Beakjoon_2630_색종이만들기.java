44b68e8ffa783dcca66cc09a1c4676a1129fea18
soun <sozzykim@gmail.com>
2024-10-28 00:30:38 +0900

    1028 ssafy

--- Algo-problem_workspace/Algo_Problem/src/beakjoon/Beakjoon_2630_색종이만들기.java
+++ Algo-problem_workspace/Algo_Problem/src/beakjoon/Beakjoon_2630_색종이만들기.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class Beakjoon_2630_색종이만들기 {
 	static int[][] map;
-	
+	static int blueCount = 0;
+	static int whiteCount = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		map = new int [N][N];
-		
+		map = new int[N][N];
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				map[i][j] = sc.nextInt();
@@ -22,68 +24,49 @@ public class Beakjoon_2630_색종이만들기 {
 //			}
 //			System.out.println();
 //		}
-		cutAndCheck(N);
-		System.out.println(count);
-		
-		
-		
-		
+		cut(0, 0, N);
+//		System.out.println("whiteCount: " + whiteCount);
+//		System.out.println("blueCount: " + blueCount);
+		System.out.println(whiteCount);
+		System.out.println(blueCount);
 	}
-	static int count = 0;
-	private static void cutAndCheck(int r, int c, int n) {
-		if(n == 1) {
-			count += 4;
-			return;
-		}
-		if(n % 2 != 0) return;
-		System.out.println("재귀호출! : " + n);
-		int hn = n/2;
-		for (int i = 0; i < hn ; i++) {
-			for (int j = 0; j < hn; j++) {
-				if(map[0][0] != map[i][j]) {
-					System.out.println( "시작 색은 "+ map[0][0] + " 인데 지금 색은 " + map[i][j]);
-					cutAndCheck(hn);
-					break;
-				}
-			}
-		}
-		System.out.println("색이 모두 " + map[0][0] +" 같아서 통과 count :" + count);
-		count++;
-		for (int i = hn; i < n ; i++) {
-			for (int j = 0; j < hn; j++) {
-				if(map[hn][0] != map[i][j]) {
-					System.out.println( "시작 색은 "+ map[hn][0] + " 인데 지금 색은 " + map[i][j]);
-					cutAndCheck(hn);
-					break;
-				}
-			}
-		}
-		System.out.println("색이 모두 같아서 통과 count :" + count);
-		count++;
-		for (int i = 0; i < hn ; i++) {
-			for (int j = hn; j < n; j++) {
-				if(map[0][hn] != map[i][j]) {
-					System.out.println( "시작 색은 "+ map[0][hn] + " 인데 지금 색은 " + map[i][j]);
-					cutAndCheck(hn);
-					break;
-				}
-			}
-		}
-		System.out.println("색이 모두 같아서 통과 count :" + count);
-		count++;
-		for (int i = hn; i < n ; i++) {
-			for (int j = hn; j < n; j++) {
-				if(map[hn][hn] != map[i][j]) {
-					System.out.println( "시작 색은 "+ map[hn][hn] + " 인데 지금 색은 " + map[i][j]);
-					cutAndCheck(hn);
-					break;
-				}
-			}
-		}
-		System.out.println("색이 모두 같아서 통과 count :" + count);
-		count++;
+
+	private static void cut(int r, int c, int size) {
+//		if(size == 1) {
+//			return;
+//		}
 		
+		boolean check = check(r, c, size); // 같은 색인지 확인하기
+		int newSize = size/2;
+		if (!check) { // 같은 색이 아니라면 분할하기
+			cut( r, c, newSize); // 왼쪽 위
+			cut( r , c+ newSize, newSize); // 오른쪽 위
+			cut( r+newSize , c, newSize); // 왼쪽 아래 
+			cut( r+newSize, c+newSize , newSize); // 오른쪽 아래
+		}
 		
-		
+
 	}
+
+	private static boolean check(int r, int c, int size) {// 같은 색인지 확인
+//		System.out.println(r + " " + c + " " + size);
+		int color = map[r][c];
+
+
+		for (int i = r; i < r+size; i++) {
+			for (int j = c; j < c+size; j++) {
+				if (map[i][j] != color) {
+//					System.out.println(color + " 와 같은 색 아니구만");
+					return false;
+				}
+			}
+		}
+		if (color == 1) {
+			blueCount += 1;
+		} else {
+			whiteCount += 1;
+		}
+		return true;
+	}
+
 }
